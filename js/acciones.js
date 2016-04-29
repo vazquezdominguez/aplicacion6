d7a80836a7089a3a99427ae628e1e2b4c7e3e34a
vazquezdominguez <jesusvaz826@gmail.com>
2016-04-29 11:14:51 -0500

    subida2

--- js/acciones.js
+++ js/acciones.js
@@ -4,13 +4,13 @@ $(document).ready(function (e){
 	document.addEventListener("deviceready",function(){
 		
 $('#beep').tap(function(){
-	navigator.notification.beep (1);
+	navigator.notification.beep (2);
 });//tap beep
 
 $('#vibrar').tap(function(){
 	navigator.notification.vibrate(1000);
 });//tap vibrar
 
-	}.false);//deviceready
+	},false);//deviceready
 });//ready
 	
