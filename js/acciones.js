e9599eba78a3e7cf7ff0002c9047d0ede684d7ff
vazquezdominguez <jesusvaz826@gmail.com>
2016-05-03 09:48:30 -0500

    Revert "subida2"
    This reverts commit d7a80836a7089a3a99427ae628e1e2b4c7e3e34a.

--- js/acciones.js
+++ js/acciones.js
@@ -4,13 +4,13 @@ $(document).ready(function (e){
 	document.addEventListener("deviceready",function(){
 		
 $('#beep').tap(function(){
-	navigator.notification.beep (2);
+	navigator.notification.beep (1);
 });//tap beep
 
 $('#vibrar').tap(function(){
 	navigator.notification.vibrate(1000);
 });//tap vibrar
 
-	},false);//deviceready
+	}.false);//deviceready
 });//ready
 	
